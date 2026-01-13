440c10f89104254f01bcfad189d1c912b2b19feb
J-Emer <jemerson6989@gmail.com>
2026-01-12 19:53:01 -0600

    Updated ReslolutionManager

--- Managers/ResolutionManager.cs
+++ Managers/ResolutionManager.cs
@@ -15,11 +15,10 @@ namespace EngineLite.Engine.Managers
         public Resolution ActiveResolution { get; private set; }
         private Resolution _normalResolution;
         private Resolution _fullScreenResolution;
-        private Resolution _borderedFullScreenResolution;
-        private bool _isFullScreen = false;
+        private bool IsFullScreen = false;
+        private readonly List<Resolution> _availableResolutions = new();
         public event Action<Resolution> OnResolutionChanged;
 
-        private readonly List<Resolution> _availableResolutions = new();
 
         public ResolutionManager(GraphicsDeviceManager graphicsDevice)
         {
@@ -28,14 +27,50 @@ namespace EngineLite.Engine.Managers
             _normalResolution = SettingsManager.Instance.Settings.DefaultResolution;
             ActiveResolution = _normalResolution;    
 
+            foreach (var item in _graphicsDeviceManager.GraphicsDevice.Adapter.SupportedDisplayModes)
+            {
+                _availableResolutions.Add(new Resolution(item.Width, item.Height, false));
+            }
+
+            int _fullWidth = _graphicsDeviceManager.GraphicsDevice.Adapter.CurrentDisplayMode.Width;
+            int _fullHeight = _graphicsDeviceManager.GraphicsDevice.Adapter.CurrentDisplayMode.Height;
+            _fullScreenResolution = new Resolution(_fullWidth, _fullHeight, true);
+
+            Instance = this;
+
+            HandleResolution();
+
+            Console.WriteLine($"Resolution Manager: {ActiveResolution}");
+        }
+
+        public void ToggleFullScreen()
+        {
+            IsFullScreen = !IsFullScreen;
+
+            if(IsFullScreen)
+            {
+                ActiveResolution = _fullScreenResolution;
+            }
+            else
+            {
+                ActiveResolution = _normalResolution;
+            }
+
+            HandleResolution();
+        }
+
+        public void SetResolution(Resolution _resolution)
+        {
+            ActiveResolution = _resolution;
+            HandleResolution();
+        }
+        private void HandleResolution()
+        {
             _graphicsDeviceManager.PreferredBackBufferWidth = ActiveResolution.Width;
             _graphicsDeviceManager.PreferredBackBufferHeight = ActiveResolution.Height;
             _graphicsDeviceManager.IsFullScreen = ActiveResolution.IsFullScreen;
             _graphicsDeviceManager.ApplyChanges();
-
-            Instance = this;
-
-            Console.WriteLine($"Resolution Manager: {ActiveResolution}");
+            OnResolutionChanged?.Invoke(ActiveResolution);
         }
 
         public string GetActiveResolution_String() => ActiveResolution.ToString();
